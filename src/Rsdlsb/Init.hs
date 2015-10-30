14819c72b3d06bfc6260e8dac6cbd5f913f5079b
Raveline <eraveline@gmail.com>
2015-10-30 08:49:13 +0100

    Test code

--- src/Rsdlsb/Init.hs
+++ src/Rsdlsb/Init.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 module Rsdlsb.Init (
     run
@@ -8,66 +9,43 @@ import Foreign.C.String (CString, peekCString, withCAString)
 import Foreign.Ptr (nullPtr)
 import GHC.Word (Word32)
 import Data.Bits ((.|.))
-import qualified Graphics.UI.SDL as SDL
-import qualified Graphics.UI.SDL.Image as Image
+import Linear (V4(..))
+import qualified SDL as SDL
+import SDL (($=))
 
 type ScreenSize = (CInt, CInt)
-type Risky a = Either String a
-
-initializeSDL :: [Word32] -> IO (Risky ())
-initializeSDL flags = do
-    initSuccess <- SDL.init $ foldl (.|.) 0 flags
-    return $ if initSuccess < 0 then Left "SDL could not initialize !" else Right ()
 
 withSDL :: IO () -> IO ()
 withSDL op = do
-    initializeSDL [SDL.SDL_INIT_VIDEO] >>= either throwSDLError return
+    SDL.initialize [SDL.InitEverything]
     op
     SDL.quit
 
-logWarning :: Risky Bool -> IO Bool
-logWarning = either (\x -> print x >> return False) return
-
 withWindow :: String -> ScreenSize -> (SDL.Window -> IO ()) -> IO ()
 withWindow title size op = do
-    window <- createWindow title size >>= either throwSDLError return
+    window <- createWindow title size
     op window
     SDL.destroyWindow window
 
 withRenderer :: (SDL.Renderer -> IO ()) -> SDL.Window -> IO ()
 withRenderer operation window = do
-    _ <- setHint "SDL_RENDER_SCALE_QUALITY" "1" >>= logWarning
-    renderer <- createRenderer window (-1) [SDL.SDL_RENDERER_ACCELERATED] >>= either throwSDLError return
+    _ <- SDL.setHintWithPriority SDL.OverridePriority SDL.HintRenderScaleQuality SDL.ScaleLinear
+    renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
     operation renderer
     SDL.destroyRenderer renderer
-
-createRenderer :: SDL.Window -> CInt -> [Word32] -> IO (Risky SDL.Renderer)
-createRenderer window index flags = do
-    renderer <- SDL.createRenderer window index $ foldl (.|.) 0 flags
-    return $ if renderer == nullPtr then Left "Renderer could not be created !" else Right renderer
-
-setHint :: String -> String -> IO (Risky Bool)
-setHint hint value = do
-    result <- withCAString2 hint value SDL.setHint
-    return $ if not result then Left "Warning: Linear texture filtering not enabled !" else Right result
 
 withCAString2 :: String -> String -> (CString -> CString -> IO a) -> IO a
 withCAString2 a b op = withCAString a $ \a' -> withCAString b $ op a'
 
-throwSDLError :: String -> IO a
-throwSDLError message = do
-    errorString <- SDL.getError >>= peekCString
-    fail (message ++ " SDL_ERROR: " ++ errorString)
-
-createWindow :: String -> ScreenSize -> IO (Risky SDL.Window)
-createWindow title (w, h) = withCAString title $ \ctitle -> do
-    window <- SDL.createWindow ctitle SDL.SDL_WINDOWPOS_UNDEFINED SDL.SDL_WINDOWPOS_UNDEFINED w h SDL.SDL_WINDOW_SHOWN
-    return $ if window == nullPtr then Left "Window could not be created !" else Right window
+createWindow :: String -> ScreenSize -> IO SDL.Window
+createWindow title (w, h) = SDL.createWindow "Test" SDL.defaultWindow -- TODO: replace defaultWindow
 
 inWindow :: String -> ScreenSize -> (SDL.Window -> IO ()) -> IO ()
 inWindow name size = withSDL . withWindow name size
 
 run :: IO ()
 run = inWindow "Test" (800, 600) $ withRenderer $ \renderer -> do
-    _ <- SDL.setRenderDrawColor renderer 0xFF 0xFF 0xFF 0xFF
+    SDL.rendererDrawColor renderer $= V4 0 0 255 255
+    SDL.clear renderer
+    SDL.present renderer
     SDL.delay 2000
